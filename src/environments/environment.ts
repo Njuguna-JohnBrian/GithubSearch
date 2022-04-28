91134b8dc834a5adb1da6ea9d1265900a81f7653
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-04-29 01:38:44 +0300

    add remote

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_USERS: "https://api.github.com/users/",
   API_SEARCH_REPOS: "https://api.github.com/search/repositories?q=",
-  ACCESS_TOKEN: "",
+  ACCESS_TOKEN: "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",
 };
 
 /*
