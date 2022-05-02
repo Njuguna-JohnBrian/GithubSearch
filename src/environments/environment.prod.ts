167d8e4565303b249c4db5d86430577248cbd425
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-05-02 16:55:37 +0300

    Add ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  API_USERS: "https://api.github.com/users/",
-  API_SEARCH_REPOS: "https://api.github.com/search/repositories?q=",
-  ACCESS_TOKEN: "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"
-};
