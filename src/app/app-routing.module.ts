99b4eb3f1c4e6d3aa26bd2ac1de5bc9dddc6200e
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-05-02 12:00:50 +0300

    Set up privarte configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ReposComponent } from './components/repos/repos.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { ReposComponent } from "./components/repos/repos.component";
 
 const routes: Routes = [
-  { path: 'profile', component: ProfileComponent},
-  { path: 'repos', component: ReposComponent},
+  {path: '', redirectTo:'/profile', pathMatch:"full"},
+  { path: "profile", component: ProfileComponent },
+  { path: "repos", component: ReposComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_USERS: "https://api.github.com/users/",
   API_SEARCH_REPOS: "https://api.github.com/search/repositories?q=",
-  ACCESS_TOKEN: ""
+  ACCESS_TOKEN: "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"
 };
