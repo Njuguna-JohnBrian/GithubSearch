a0889a7cea35a662c3c4da238d9abefacf04a3e0
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-05-02 10:10:30 +0300

    Add creds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_USERS: "https://api.github.com/users/",
   API_SEARCH_REPOS: "https://api.github.com/search/repositories?q=",
-  ACCESS_TOKEN: "",
+  ACCESS_TOKEN: "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",
 };
 
 /*
