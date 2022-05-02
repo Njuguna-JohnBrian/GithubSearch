8df1d84af561a546809ff155ed27ff955ac40267
Njuguna-JohnBrian <njugunajb96@gmail.com>
2022-05-02 15:51:12 +0300

    change deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_USERS: "https://api.github.com/users/",
   API_SEARCH_REPOS: "https://api.github.com/search/repositories?q=",
-  ACCESS_TOKEN: "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",
+  ACCESS_TOKEN: "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",
 };
 
 /*
